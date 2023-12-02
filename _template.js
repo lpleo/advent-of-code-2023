0b6416742992a3887f54fb0b713758d771f23978
Leonardo <leli.cons@bene.it>
2023-12-02 18:18:42 +0100

    refactor Â_template

--- _template.js
+++ _template.js
@@ -3,12 +3,11 @@ let handleFile = require("../common/handleFile");
 
 
 let test1 = []
-
 let test2 = []
 
 console.log("PT1 TEST: " + partOne(test1));
 console.log("PT1 RUN : " + partOne(handleFile.readDataFile1(path.basename(__filename, '.js'))))
-console.log("PT2 TEST: " + partTwo(test2))
+console.log("PT2 TEST: " + partTwo(test2.length === 0 ? test1 : test2))
 console.log("PT2 RUN : " + partTwo(handleFile.readDataFile2(path.basename(__filename, '.js'))))
 
 function partOne(rows) {
